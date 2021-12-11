0b9714b21d6a5303cec3cc8a6670d7f08822c110
Guigalaverna <guigalaverna@gmail.com>
2021-12-11 15:07:04 -0300

    Adicionando os icones

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -2,15 +2,55 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { Dashboard } from '../screens/Dashboard';
 import { Register } from '../screens/Register';
+import { DefaultNavigatorOptions } from '@react-navigation/native';
+import theme from '../global/styles/theme';
+import { Platform } from 'react-native';
+
+import { Feather } from '@expo/vector-icons';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppRoutes() {
   return (
-    <Navigator screenOptions={{ headerShown: false }}>
-      <Screen name="Listagem" component={Dashboard} />
-      <Screen name="Cadastrar" component={Register} />
-      <Screen name="Resumo" component={Register} />
+    <Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: theme.colors.secondary,
+        tabBarInactiveTintColor: theme.colors.text,
+        tabBarLabelPosition: 'beside-icon',
+        tabBarStyle: {
+          height: 72,
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+        },
+      }}
+    >
+      <Screen
+        name="Listagem"
+        component={Dashboard}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="list" color={color} size={size} />
+          ),
+        }}
+      />
+      <Screen
+        name="Cadastrar"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="dollar-sign" color={color} size={size} />
+          ),
+        }}
+      />
+      <Screen
+        name="Resumo"
+        component={Register}
+        options={{
+          tabBarIcon: ({ size, color }) => (
+            <Feather name="pie-chart" color={color} size={size} />
+          ),
+        }}
+      />
     </Navigator>
   );
 }
