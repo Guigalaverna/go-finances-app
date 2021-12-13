356c79dd4aac38c132e1cc3f18dc0a2d3d6c49dd
Guigalaverna <guigalaverna@gmail.com>
2021-12-13 02:45:34 -0300

    Calculando os valores das transações

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,6 +1,9 @@
-import { useAsyncStorage } from '@react-native-async-storage/async-storage';
-import { useFocusEffect } from '@react-navigation/native';
 import React, { useCallback, useEffect, useState } from 'react';
+import { ActivityIndicator } from 'react-native';
+import { useAsyncStorage } from '@react-native-async-storage/async-storage';
+
+import { useFocusEffect } from '@react-navigation/native';
+import { useTheme } from 'styled-components';
 
 import { HighlightCard } from '../../components/HighlightCard';
 import {
@@ -23,23 +26,69 @@ import {
   Title,
   TransactionList,
   LogoutButton,
+  LoadContainer,
 } from './styles';
 
 export interface DataListProps extends TransactionCardProps {
   id: string;
 }
 
+interface HighlightProps {
+  amount: string;
+  lastTransaction: string;
+}
+
+interface HighlightData {
+  incomes: HighlightProps;
+  outcomes: HighlightProps;
+  total: HighlightProps;
+}
+
 export function Dashboard() {
-  const [data, setData] = useState<DataListProps[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [transactions, setTransactions] = useState<DataListProps[]>([]);
+  const [highlightData, setHighlightData] = useState<HighlightData>(
+    {} as HighlightData
+  );
+
   const AsyncStorage = useAsyncStorage('@gofinances:transactions');
+  const theme = useTheme();
+
+  function getLastTransactionDate(
+    collection: DataListProps[],
+    type: 'income' | 'outcome'
+  ) {
+    const lastTransaction = new Date(
+      Math.max.apply(
+        Math,
+        collection
+          .filter(transaction => transaction.type === type)
+          .map(transaction => new Date(transaction.date).getTime())
+      )
+    );
+
+    return `${lastTransaction.getDate()} de ${lastTransaction.toLocaleString(
+      'pt-BR',
+      { month: 'long' }
+    )}`;
+  }
 
   async function loadTransactions() {
     const response = await AsyncStorage.getItem();
-    const transactions: DataListProps[] = response ? JSON.parse(response) : [];
+    const transactions = response ? JSON.parse(response) : [];
+
+    let entriesTotal = 0;
+    let expensiveTotal = 0;
 
     const transactionsFormatted: DataListProps[] = transactions.map(
-      transaction => {
-        const amount = Number(transaction.amount).toLocaleString('pt-BR', {
+      (item: DataListProps) => {
+        if (item.type === 'income') {
+          entriesTotal += Number(item.amount);
+        } else {
+          expensiveTotal += Number(item.amount);
+        }
+
+        const amount = Number(item.amount).toLocaleString('pt-BR', {
           style: 'currency',
           currency: 'BRL',
         });
@@ -48,26 +97,63 @@ export function Dashboard() {
           day: '2-digit',
           month: '2-digit',
           year: '2-digit',
-        }).format(new Date(transaction.date));
+        }).format(new Date(item.date));
 
         return {
-          ...transaction,
+          id: item.id,
+          name: item.name,
           amount,
+          type: item.type,
+          category: item.category,
           date,
         };
       }
     );
 
-    setData(transactionsFormatted);
-    console.log(data);
+    setTransactions(transactionsFormatted);
+
+    const lastTransactionEntries = getLastTransactionDate(
+      transactions,
+      'income'
+    );
+    const lastTransactionExpensives = getLastTransactionDate(
+      transactions,
+      'outcome'
+    );
+    const totalInterval = `01 a ${lastTransactionExpensives}`;
+
+    const total = entriesTotal - expensiveTotal;
+
+    setHighlightData({
+      incomes: {
+        amount: entriesTotal.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }),
+        lastTransaction: `Última entrada dia ${lastTransactionEntries}`,
+      },
+      outcomes: {
+        amount: expensiveTotal.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }),
+        lastTransaction: `Última saída dia ${lastTransactionExpensives}`,
+      },
+      total: {
+        amount: total.toLocaleString('pt-BR', {
+          style: 'currency',
+          currency: 'BRL',
+        }),
+        lastTransaction: totalInterval,
+      },
+    });
+
+    setIsLoading(false);
   }
 
-  async function handleDeleteTransaction(id: string) {
-    const newData = data.filter(transaction => transaction.id !== id);
-
-    setData(newData);
-    await AsyncStorage.setItem(JSON.stringify(newData));
-  }
+  useEffect(() => {
+    loadTransactions();
+  }, []);
 
   useFocusEffect(
     useCallback(() => {
@@ -77,61 +163,62 @@ export function Dashboard() {
 
   return (
     <Container>
-      <Header>
-        <UserWrapper>
-          <UserInfo>
-            <Photo
-              source={{
-                uri: 'https://github.com/Guigalaverna.png',
-              }}
+      {isLoading ? (
+        <LoadContainer>
+          <ActivityIndicator color={theme.colors.primary} size="large" />
+        </LoadContainer>
+      ) : (
+        <>
+          <Header>
+            <UserWrapper>
+              <UserInfo>
+                <Photo
+                  source={{ uri: 'https://github.com/Guigalaverna.png' }}
+                />
+                <User>
+                  <UserGreeting>Olá,</UserGreeting>
+                  <UserName>Guilherme</UserName>
+                </User>
+              </UserInfo>
+
+              <LogoutButton onPress={() => {}}>
+                <Icon name="power" />
+              </LogoutButton>
+            </UserWrapper>
+          </Header>
+
+          <HighlightCards>
+            <HighlightCard
+              type="income"
+              title="Entradas"
+              amount={highlightData.incomes.amount}
+              lastTransaction={highlightData.incomes.lastTransaction}
             />
-            <User>
-              <UserGreeting>Olá,</UserGreeting>
-              <UserName>Guilherme</UserName>
-            </User>
-          </UserInfo>
-
-          <LogoutButton onPress={() => {}}>
-            <Icon name="power" />
-          </LogoutButton>
-        </UserWrapper>
-      </Header>
-
-      <HighlightCards>
-        <HighlightCard
-          type="income"
-          title="Entradas"
-          amount="R$ 17.400,00"
-          lastTransaction="Última entrada dia 13 de abril"
-        />
-        <HighlightCard
-          type="outcome"
-          title="Saídas"
-          amount="R$ 1.259,00"
-          lastTransaction="Última saída dia 13 de abril"
-        />
-        <HighlightCard
-          type="total"
-          title="Total"
-          amount="R$ 16.141,00"
-          lastTransaction="01 à 16 de abril"
-        />
-      </HighlightCards>
-
-      <Transactions>
-        <Title>Listagem</Title>
-
-        <TransactionList
-          data={data}
-          keyExtractor={item => item.id}
-          renderItem={({ item }) => (
-            <TransactionCard
-              handleDeleteTransaction={() => handleDeleteTransaction(item.id)}
-              data={item}
+            <HighlightCard
+              type="outcome"
+              title="Saídas"
+              amount={highlightData.outcomes.amount}
+              lastTransaction={highlightData.outcomes.lastTransaction}
             />
-          )}
-        />
-      </Transactions>
+            <HighlightCard
+              type="total"
+              title="Total"
+              amount={highlightData.total.amount}
+              lastTransaction={highlightData.total.lastTransaction}
+            />
+          </HighlightCards>
+
+          <Transactions>
+            <Title>Listagem</Title>
+
+            <TransactionList
+              data={transactions}
+              keyExtractor={item => item.id}
+              renderItem={({ item }) => <TransactionCard data={item} />}
+            />
+          </Transactions>
+        </>
+      )}
     </Container>
   );
 }
--- src/screens/Register/index.tsx
+++ src/screens/Register/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Modal, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 import * as Yup from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
